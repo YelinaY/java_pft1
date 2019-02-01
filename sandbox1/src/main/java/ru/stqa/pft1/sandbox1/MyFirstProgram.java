6f6f85f98886ecc042b2e948860db1a7e4255cfb
YelinaY <eelina1205@gmail.com>
2019-02-01 22:29:53 +0200

    Задание 2: Созданы классы Point, Rectangle, Square (использованы новые функции, методы, объекты и классы)

--- sandbox1/src/main/java/ru/stqa/pft1/sandbox1/MyFirstProgram.java
+++ sandbox1/src/main/java/ru/stqa/pft1/sandbox1/MyFirstProgram.java
@@ -3,24 +3,23 @@ package ru.stqa.pft1.sandbox1;
 public class MyFirstProgram {
   public static void main(String[] args) {
     hello("Alex");
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.area()); //вызов метода s.area
 
-    double len = 5;
-    System.out.println("Площадь квадрата со стороной " + len + "=" + area(len));
-    double a=4;
-    double b=6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b+ " = " + area(a,b));
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area()); //вызов метода r.area
+
+    Point p = new Point(2,4,6,8);
+    System.out.println("Расстояние между двумя точками  с координатами x1y1 и x2y2 = " + p.distance ()); //вызов метода p.distance ());
+
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello," + somebody);
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
-public static double area (double a, double b){
-    return a*b;
-}
+
+
 }
 
 //Hello,Alex
---
+++ sandbox1/src/main/java/ru/stqa/pft1/sandbox1/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft1.sandbox1;
+
+public class Square {
+  public double l;
+
+  public Square (double l){
+    this.l = l;
+  }
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
+
