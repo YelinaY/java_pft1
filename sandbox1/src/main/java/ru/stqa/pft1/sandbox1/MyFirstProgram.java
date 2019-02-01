a3c7778179a47d23e730466da5c767fe1bad35bf
YelinaY <eelina1205@gmail.com>
2019-02-01 14:07:23 +0200

    Изменены настройки форматирования

--- sandbox1/src/main/java/ru/stqa/pft1/sandbox1/MyFirstProgram.java
+++ sandbox1/src/main/java/ru/stqa/pft1/sandbox1/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft1.sandbox1;
 
 public class MyFirstProgram {
-public static void main(String[]args){
-System.out.println ("Hello, word!!!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello, word!!!");
+  }
 }
