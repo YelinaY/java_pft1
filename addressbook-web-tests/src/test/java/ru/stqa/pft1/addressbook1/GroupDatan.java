6ddcc8f1bb82837ad1bae7bc22ad834655a9688c
YelinaY <eelina1205@gmail.com>
2019-02-04 11:46:52 +0200

    4 задание - рефакторинг Parametr Object

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/GroupDatan.java
@@ -0,0 +1,103 @@
+package ru.stqa.pft1.addressbook1;
+
+public class GroupDatan {
+  private final String usermiddlename;
+  private final String userlastname;
+  private final String usernickname;
+  private final String userCompany;
+  private final String userAddress;
+  private final String userHome;
+  private final String userMobile;
+  private final String userWorkingPhone;
+  private final String userFax;
+  private final String userMail;
+  private final String userMail2;
+  private final String userMail3;
+  private final String userHomepage;
+  private final String userAddress2;
+  private final String userPhone2;
+  private final String userNotes;
+
+  public GroupDatan(String usermiddlename, String userlastname, String usernickname, String userCompany, String userAddress, String userHome, String userMobile, String userWorkingPhone, String userFax, String userMail, String userMail2, String userMail3, String userHomepage, String userAddress2, String userPhone2, String userNotes) {
+    this.usermiddlename = usermiddlename;
+    this.userlastname = userlastname;
+    this.usernickname = usernickname;
+    this.userCompany = userCompany;
+    this.userAddress = userAddress;
+    this.userHome = userHome;
+    this.userMobile = userMobile;
+    this.userWorkingPhone = userWorkingPhone;
+    this.userFax = userFax;
+    this.userMail = userMail;
+    this.userMail2 = userMail2;
+    this.userMail3 = userMail3;
+    this.userHomepage = userHomepage;
+    this.userAddress2 = userAddress2;
+    this.userPhone2 = userPhone2;
+    this.userNotes = userNotes;
+  }
+
+  public String getUsermiddlename() {
+    return usermiddlename;
+  }
+
+  public String getUserlastname() {
+    return userlastname;
+  }
+
+  public String getUsernickname() {
+    return usernickname;
+  }
+
+  public String getUserCompany() {
+    return userCompany;
+  }
+
+  public String getUserAddress() {
+    return userAddress;
+  }
+
+  public String getUserHome() {
+    return userHome;
+  }
+
+  public String getUserMobile() {
+    return userMobile;
+  }
+
+  public String getUserWorkingPhone() {
+    return userWorkingPhone;
+  }
+
+  public String getUserFax() {
+    return userFax;
+  }
+
+  public String getUserMail() {
+    return userMail;
+  }
+
+  public String getUserMail2() {
+    return userMail2;
+  }
+
+  public String getUserMail3() {
+    return userMail3;
+  }
+
+  public String getUserHomepage() {
+    return userHomepage;
+  }
+
+  public String getUserAddress2() {
+    return userAddress2;
+  }
+
+  public String getUserPhone2() {
+    return userPhone2;
+  }
+
+  public String getUserNotes() {
+    return userNotes;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/NewAddressBookCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/NewAddressBookCreationTests.java
@@ -44,7 +44,7 @@ public class NewAddressBookCreationTests {
     openAddressBook();
     addNewAddressBook();
     userFirstName("Eлена");
-    new FillForm().invoke("Y/Y", "Yelina", "Lina", "TCWD", "Paris, Royal sq.", "+1111111111", "+222222222", "+33333333", "+44444444", "mail@mail.com", "mail1@mail.com", "mail3@mail.com", "URL", "Minsk", "+5555555555", "Notes");
+    new FillForm().invoke(new GroupDatan("Y/Y", "Yelina", "Lina", "TCWD", "Paris, Royal sq.", "+1111111111", "+222222222", "+33333333", "+44444444", "mail@mail.com", "mail1@mail.com", "mail3@mail.com", "URL", "Minsk", "+5555555555", "Notes"));
     submitNewAddressBook();
 
   }
@@ -73,55 +73,55 @@ public class NewAddressBookCreationTests {
   }
 
   private class FillForm {
-    public void invoke(String usermiddlename, String userlastname, String usernickname, String userCompany, String userAddress, String userHome, String userMobile, String userWorkingPhone, String userFax, String userMail, String userMail2, String userMail3, String userHomepage, String userAddress2, String userPhone2, String userNotes) {
+    public void invoke(GroupDatan groupDatan) {
       wd.findElement(By.name("middlename")).click();
       wd.findElement(By.name("middlename")).clear();
-      wd.findElement(By.name("middlename")).sendKeys(usermiddlename);
+      wd.findElement(By.name("middlename")).sendKeys(groupDatan.getUsermiddlename());
       wd.findElement(By.name("lastname")).click();
       wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(userlastname);
+      wd.findElement(By.name("lastname")).sendKeys(groupDatan.getUserlastname());
       wd.findElement(By.name("nickname")).click();
       wd.findElement(By.name("nickname")).clear();
-      wd.findElement(By.name("nickname")).sendKeys(usernickname);
+      wd.findElement(By.name("nickname")).sendKeys(groupDatan.getUsernickname());
       wd.findElement(By.name("company")).click();
       wd.findElement(By.name("company")).clear();
-      wd.findElement(By.name("company")).sendKeys(userCompany);
+      wd.findElement(By.name("company")).sendKeys(groupDatan.getUserCompany());
       wd.findElement(By.name("address")).click();
       wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(userAddress);
+      wd.findElement(By.name("address")).sendKeys(groupDatan.getUserAddress());
       wd.findElement(By.name("home")).click();
       wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(userHome);
+      wd.findElement(By.name("home")).sendKeys(groupDatan.getUserHome());
       wd.findElement(By.name("mobile")).click();
       wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(userMobile);
+      wd.findElement(By.name("mobile")).sendKeys(groupDatan.getUserMobile());
       wd.findElement(By.name("work")).click();
       wd.findElement(By.name("work")).clear();
-      wd.findElement(By.name("work")).sendKeys(userWorkingPhone);
+      wd.findElement(By.name("work")).sendKeys(groupDatan.getUserWorkingPhone());
       wd.findElement(By.name("fax")).click();
       wd.findElement(By.name("fax")).clear();
-      wd.findElement(By.name("fax")).sendKeys(userFax);
+      wd.findElement(By.name("fax")).sendKeys(groupDatan.getUserFax());
       wd.findElement(By.name("email")).click();
       wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(userMail);
+      wd.findElement(By.name("email")).sendKeys(groupDatan.getUserMail());
       wd.findElement(By.name("email2")).click();
       wd.findElement(By.name("email2")).clear();
-      wd.findElement(By.name("email2")).sendKeys(userMail2);
+      wd.findElement(By.name("email2")).sendKeys(groupDatan.getUserMail2());
       wd.findElement(By.name("email3")).click();
       wd.findElement(By.name("email3")).clear();
-      wd.findElement(By.name("email3")).sendKeys(userMail3);
+      wd.findElement(By.name("email3")).sendKeys(groupDatan.getUserMail3());
       wd.findElement(By.name("homepage")).click();
       wd.findElement(By.name("homepage")).clear();
-      wd.findElement(By.name("homepage")).sendKeys(userHomepage);
+      wd.findElement(By.name("homepage")).sendKeys(groupDatan.getUserHomepage());
       wd.findElement(By.name("address2")).click();
       wd.findElement(By.name("address2")).clear();
-      wd.findElement(By.name("address2")).sendKeys(userAddress2);
+      wd.findElement(By.name("address2")).sendKeys(groupDatan.getUserAddress2());
       wd.findElement(By.name("phone2")).click();
       wd.findElement(By.name("phone2")).clear();
-      wd.findElement(By.name("phone2")).sendKeys(userPhone2);
+      wd.findElement(By.name("phone2")).sendKeys(groupDatan.getUserPhone2());
       wd.findElement(By.name("notes")).click();
       wd.findElement(By.name("notes")).clear();
-      wd.findElement(By.name("notes")).sendKeys(userNotes);
+      wd.findElement(By.name("notes")).sendKeys(groupDatan.getUserNotes());
     }
   }
 }
