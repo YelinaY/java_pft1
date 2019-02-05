1557498cb591c58004ee3ee5b3f2dcad5410d66b
YelinaY <eelina1205@gmail.com>
2019-02-05 07:07:47 +0200

    Задание №5: Реализовать двухуровневую архитектуру тестового набора +NewAddressBook

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/NewAddressBookCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/NewAddressBookCreationTests.java
@@ -40,73 +40,84 @@ public class NewAddressBookCreationTests {
   }
 
   @Test
-  public void testUntitledTestCase1() throws Exception {
-    wd.get("http://localhost/addressbook/group.php");
-   wd.findElement(By.linkText("add new")).click();
-   wd.findElement(By.name("firstname")).click();
+  public void testNewAddressBookCreation() throws Exception {
+    goToGroupPage();
+    initAddressBookCreation();
+    fillAddressBookForm("Eлена", "Y/Y", "Yelina", "Lina", "TCWD", "Paris, Royal sq.", "+1111111111", "+222222222", "+33333333", "+44444444", "mail@mail.com", "mail1@mail.com", "mail3@mail.com", "URL", "Minsk", "+5555555555", "Notes");
+    submitNewAddressBook();
+  }
+
+  private void submitNewAddressBook() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillAddressBookForm(String userfirstname, String usermiddlename, String userlastname, String usernickname, String usercompany, String useraddress, String userhomephone, String usermobilephone, String userworkphone, String userfax, String useremail, String useremail2, String useremail3, String userhomepage, String useraddress2, String userphone2, String usernotes) {
+    wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Eлена");
+    wd.findElement(By.name("firstname")).sendKeys(userfirstname);
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Y/Y");
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Yelena");
+    wd.findElement(By.name("middlename")).sendKeys(usermiddlename);
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Yelina");
+    wd.findElement(By.name("lastname")).sendKeys(userlastname);
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("Lina");
+    wd.findElement(By.name("nickname")).sendKeys(usernickname);
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("TCWD");
+    wd.findElement(By.name("company")).sendKeys(usercompany);
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Paris, Royal sq.");
+    wd.findElement(By.name("address")).sendKeys(useraddress);
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("+1111111111");
+    wd.findElement(By.name("home")).sendKeys(userhomephone);
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("+222222222");
+    wd.findElement(By.name("mobile")).sendKeys(usermobilephone);
     wd.findElement(By.name("work")).click();
     wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys("+33333333");
+    wd.findElement(By.name("work")).sendKeys(userworkphone);
     wd.findElement(By.name("fax")).click();
     wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys("+44444444");
+    wd.findElement(By.name("fax")).sendKeys(userfax);
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("mail@mail.com");
+    wd.findElement(By.name("email")).sendKeys(useremail);
     wd.findElement(By.name("email2")).click();
     wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("mail1@mail.com");
+    wd.findElement(By.name("email2")).sendKeys(useremail2);
     wd.findElement(By.name("email3")).click();
-    wd.findElement(By.name("email2")).click();
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("mail2@mail.com");
     wd.findElement(By.name("email3")).click();
     wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys("mail3@mail.com");
+    wd.findElement(By.name("email3")).sendKeys(useremail3);
     wd.findElement(By.name("homepage")).click();
     wd.findElement(By.name("homepage")).clear();
-    wd.findElement(By.name("homepage")).sendKeys("URL");
+    wd.findElement(By.name("homepage")).sendKeys(userhomepage);
     wd.findElement(By.name("address2")).click();
     wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys("Minsk");
+    wd.findElement(By.name("address2")).sendKeys(useraddress2);
     wd.findElement(By.name("phone2")).click();
     wd.findElement(By.name("phone2")).clear();
-    wd.findElement(By.name("phone2")).sendKeys("+5555555555");
+    wd.findElement(By.name("phone2")).sendKeys(userphone2);
     wd.findElement(By.name("notes")).click();
     wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys("Notes");
-    wd.findElement(By.xpath("//body")).click();
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
+    wd.findElement(By.name("notes")).sendKeys(usernotes);
+  }
+
+  private void initAddressBookCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  private void goToGroupPage() {
+    wd.get("http://localhost/addressbook/group.php");
+  }
 
 
   @AfterMethod
   public void tearDown() {
     wd.quit();
   }
+
 }
