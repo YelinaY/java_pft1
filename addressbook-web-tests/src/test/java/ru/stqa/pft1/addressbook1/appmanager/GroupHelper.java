d29ea2c2422045f7ab48f6497c6e15c569df1a49
YelinaY <eelina1205@gmail.com>
2019-02-11 13:33:07 +0200

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/appmanager/GroupHelper.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft1.addressbook1.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft1.addressbook1.model.ContactData;
 import ru.stqa.pft1.addressbook1.model.GroupData;
 
 public class GroupHelper extends HelperBase {
 
-  public GroupHelper(FirefoxDriver wd) {
+  public GroupHelper(WebDriver wd) {
     super(wd);
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/appmanager/NavigationHelper.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft1.addressbook1.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft1.addressbook1.model.ContactData;
 
 public class NavigationHelper extends HelperBase {
 
-  public NavigationHelper(FirefoxDriver wd) {
+  public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
