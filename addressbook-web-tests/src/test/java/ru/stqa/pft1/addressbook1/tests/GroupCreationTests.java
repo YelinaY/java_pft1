33e9df86d78f4f3ef3e37c30f6753065b6e8ca81
YelinaY <eelina1205@gmail.com>
2019-02-17 15:00:08 +0200

    4.3 Сравнила (assert) количество групп до cоздания и после

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft1.addressbook1.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft1.addressbook1.model.GroupData;
 
@@ -8,8 +9,12 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreaton() {
+    // сравнить количество групп до добавления и после
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("Test1", null, "Test3"));
+    int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
 
   }
 
