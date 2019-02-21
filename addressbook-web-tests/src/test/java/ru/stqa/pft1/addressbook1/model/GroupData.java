c7469f7a706138a00a0525521f53133dfda344b6
YelinaY <eelina1205@gmail.com>
2019-02-21 22:20:53 +0200

    4.7  Множества: неупорядоченные коллекции
    Создание теста  сравнения групп по id для модиффикации группы
    сравнение полученного множества

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/model/GroupData.java
@@ -6,11 +6,22 @@ public class GroupData {
   private final String name;
   private final String header;
   private final String footer;
+  private final String id;
 
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
+
+  }
+
+  public GroupData(String id, String name, String header, String footer) {
+   this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+
   }
 
   public String getName() {
@@ -25,25 +36,29 @@ public class GroupData {
     return footer;
   }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
-  }
+  public String getId() {return id;}
 
   @Override
-  public boolean equals(Object o) { // сравнение объектов по атрибутам объектов
+  public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-      }
+    return Objects.equals(name, groupData.name) &&
+            Objects.equals(id, groupData.id);
+  }
 
   @Override
   public int hashCode() {
 
-    return Objects.hash(name);
+    return Objects.hash(name, id);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id +'\'' +
+            "name='" + name + '\'' +
+            '}';
   }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft1.addressbook1.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -18,14 +19,17 @@ public class GroupModificationTests extends TestBase {
        // int before = app.getGroupContactHelper().getGroupCount();
     app.getGroupContactHelper().selectGroup(before.size() -1); //выбрать последнюю группу
     app.getGroupContactHelper().initGroupModification();
-    app.getGroupContactHelper().fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+    GroupData group = new GroupData(before.get(before.size() -1).getId(), "Test1", "Test2", "Test3");
+    app.getGroupContactHelper().fillGroupForm(group);
     app.getGroupContactHelper().submitGroupModification();
     app.getGroupContactHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupContactHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() );
    // int after  = app.getGroupContactHelper().getGroupCount();
    // Assert.assertEquals(after, before); // сравнение количества групп до и после модификации
-    Assert.assertEquals(before,after); //сравниваем списки
+    before.remove(before.size() -1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); //сравниваем списки
 
 
   }
