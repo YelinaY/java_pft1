6e561163d7dccd87050f084f35bef06d3d9954df
YelinaY <eelina1205@gmail.com>
2019-03-10 20:55:01 +0200

    5.6 изменен тест AddressBookDeletionTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/AddressBookDeletion.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/AddressBookDeletion.java
@@ -1,11 +1,19 @@
 package ru.stqa.pft1.addressbook1.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft1.addressbook1.model.ContactData;
+import ru.stqa.pft1.addressbook1.model.Contacts;
+
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class AddressBookDeletion extends TestBase {
   @BeforeMethod
@@ -20,13 +28,13 @@ public class AddressBookDeletion extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.groupsContacts().gotoHomePage();
-    Set<ContactData> before = app.groupsContacts().allc();
+    Contacts before = app.groupsContacts().allc();
     ContactData deletedContact = before.iterator().next();
     app.groupsContacts().deleteContact(deletedContact);
-    Set<ContactData> after = app.groupsContacts().allc();
-    Assert.assertEquals(after.size(),before.size() -1);
-    before.remove(deletedContact);
-    Assert.assertEquals(before, after);
+    Contacts after = app.groupsContacts().allc();
+    assertEquals(after.size(),before.size() -1);
+
+    assertThat(after, equalTo(before.withOutc(deletedContact)));
   }
 
 
