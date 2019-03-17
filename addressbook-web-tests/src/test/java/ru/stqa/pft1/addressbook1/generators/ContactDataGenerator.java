5a4a71967a07b8e5760d58d52bf16e381b5130e8
YelinaY <eelina1205@gmail.com>
2019-03-17 21:37:41 +0200

    6.8. Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/generators/ContactDataGenerator.java
@@ -55,30 +55,27 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+     }
   }
-
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
-
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;\n", contact.getUserfirstname(), contact.getUserlastname()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;\n", contact.getUserfirstname(), contact.getUserlastname()));
+      }
     }
-    writer.close();
   }
-
   private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/generators/GroupDataGenerator.java
@@ -57,38 +57,36 @@ public class GroupDataGenerator {
   private void saveAsJson(List<GroupData> groups, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
-
-  }
+    try (Writer writer = new FileWriter(file)){
+      writer.write(json);
+    }
+   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
     XStream xStream = new XStream();
     xStream.processAnnotations(GroupData.class);//прочитать подсказки в классе GroupData
     String xml = xStream.toXML(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
-  }
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
+   }
 
 
   private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
-    Writer writer = new FileWriter(file);
-    for (GroupData group: groups){
-      writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+    try (Writer writer = new FileWriter(file)) {
+      for (GroupData group : groups) {
+        writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+      }
     }
-    writer.close();
   }
-
-  private List<GroupData> generateGroups(int count) {
-    List<GroupData>groups = new ArrayList<GroupData>();
-    for (int i = 0; i< count; i++) {
-      groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i)).
-              withFooter(String.format("footer %s", i)));
+    private List<GroupData> generateGroups ( int count){
+      List<GroupData> groups = new ArrayList<GroupData>();
+      for (int i = 0; i < count; i++) {
+        groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i)).
+                withFooter(String.format("footer %s", i)));
+      }
+      return groups;
     }
-    return groups;
   }
-}
 
