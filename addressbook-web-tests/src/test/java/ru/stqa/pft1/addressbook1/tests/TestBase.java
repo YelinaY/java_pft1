1edd4d9a52e847510a2e738eb88a5688d1729b5c
YelinaY <eelina1205@gmail.com>
2019-03-18 09:47:28 +0200

    6.10. Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/TestBase.java
@@ -6,7 +6,8 @@ import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft1.addressbook1.appmanager.ApplicationManager;
 
 public class TestBase {
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
