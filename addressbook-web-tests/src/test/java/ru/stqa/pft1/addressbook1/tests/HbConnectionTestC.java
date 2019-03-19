0d4442460470f6227cbd68e16ad01bc2dde6373c
YelinaY <eelina1205@gmail.com>
2019-03-19 11:03:46 +0200

    7.3 ошибка

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/HbConnectionTestC.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/HbConnectionTestC.java
@@ -7,7 +7,8 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import ru.stqa.pft1.addressbook1.model.GroupData;
+import ru.stqa.pft1.addressbook1.model.ContactData;
+
 
 import java.util.List;
 
@@ -36,9 +37,9 @@ public class HbConnectionTestC {
   public void testHbConnection(){
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    List<GroupData> result = session.createQuery("from ContactData" ).list();
-    for (GroupData group : result) {
-      System.out.println(group);
+    List<ContactData> result = session.createQuery("from ContactData" ).list();
+    for (ContactData contact : result) {
+      System.out.println(contact);
     }
     session.getTransaction().commit();
     session.close();
