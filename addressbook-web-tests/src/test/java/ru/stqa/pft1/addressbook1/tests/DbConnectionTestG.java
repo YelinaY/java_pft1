9750817c982a94b8865b6545847ae133abaf4d8a
YelinaY <eelina1205@gmail.com>
2019-03-19 07:07:32 +0200

    7.1. Драйвер базы данных. Получение данных из БД Groups&Contacts

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/DbConnectionTestG.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft1.addressbook1.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft1.addressbook1.model.GroupData;
+import ru.stqa.pft1.addressbook1.model.Groups;
+import java.sql.*;
+
+
+public class DbConnectionTestG {
+  @Test
+  public void testDbConnectionGroups() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+      Groups groups = new Groups();
+      while (rs.next()){
+       groups.add (new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name")).
+                withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(groups);
+      // Do something with the Connection
+
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/tests/TestBase.java
@@ -32,13 +32,13 @@ public class TestBase {
 
   @BeforeMethod (alwaysRun = true)
   public void  logTestStart(Method m, Object [] p){
-    logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
 
 
   }
   @AfterMethod (alwaysRun = true)
   public void  logTestStop(Method m, Object [] p){
-    logger.info("Stop test " + m.getName() + "with parameters " + Arrays.asList(p));
+    logger.info("Stop test " + m.getName() + " with parameters " + Arrays.asList(p));
 
   }
 }
