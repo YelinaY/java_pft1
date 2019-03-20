c9eb6aa9fcd87a3ba12252fa23d3b07e97b2d2ee
YelinaY <eelina1205@gmail.com>
2019-03-20 23:09:57 +0200

    7.6 частично выполнено

--- addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft1/addressbook1/model/GroupData.java
@@ -1,14 +1,12 @@
 package ru.stqa.pft1.addressbook1.model;
 
-import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @XStreamAlias("group")
 @Entity
@@ -16,19 +14,61 @@ import javax.persistence.Table;
 public class GroupData {
   @XStreamOmitField
   @Id
-  @Column (name = "group_id")
+  @Column(name = "group_id")
   private int id = Integer.MAX_VALUE;
-  @Expose
-  @Column (name ="group_name")
+  @Column(name = "group_name")
   private String name;
-  @Expose
-  @Column (name ="group_header")
+
+  @ManyToMany(mappedBy = "groups")
+  private Set<ContactData> contacts = new HashSet<ContactData>();
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
+    if (header != null ? !header.equals(groupData.header) : groupData.header != null) return false;
+    return footer != null ? footer.equals(groupData.footer) : groupData.footer == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    result = 31 * result + (header != null ? header.hashCode() : 0);
+    result = 31 * result + (footer != null ? footer.hashCode() : 0);
+    return result;
+  }
+
+  @Column(name = "group_header")
   @Type(type = "text")
   private String header;
-  @Expose
-  @Column (name ="group_footer")
+  @Column(name = "group_footer")
   @Type(type = "text")
   private String footer;
+
+  public GroupData withId(int id) {
+    this.id = id;
+    return this;
+  }
+  public GroupData withName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public GroupData withHeader(String header) {
+    this.header = header;
+    return this;
+  }
+
+  public GroupData withFooter(String footer) {
+    this.footer = footer;
+    return this;
+  }
 
   public int getId() {
     return id;
@@ -46,49 +86,16 @@ public class GroupData {
     return footer;
   }
 
-  public GroupData withId(int id) {
-    this.id = id;
-    return this;
-  }
-
-  public GroupData withName(String name) {
-    this.name = name;
-    return this;
-  }
-
-  public GroupData withHeader(String header) {
-    this.header = header;
-    return this;
-  }
-
-  public GroupData withFooter(String footer) {
-    this.footer = footer;
-    return this;
+  public Contacts getContacts() {
+    return new Contacts(contacts);
   }
 
   @Override
   public String toString() {
     return "GroupData{" +
             "id='" + id + '\'' +
-            "name='" + name + '\'' +
+            ", name='" + name + '\'' +
             '}';
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != groupData.id) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
-  }
 }
