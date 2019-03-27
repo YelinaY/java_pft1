867d6224ea7b19a3e6265b2aead29ee5dea619cd
YelinaY <eelina1205@gmail.com>
2019-03-27 21:29:13 +0200

    8.7. Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet)

--- mantis-tests/src/test/java/ru/stqa/pft1/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft1/mantis/tests/RegistrationTests.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft1.mantis.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft1.mantis.model.MailMessage;
@@ -12,9 +10,9 @@ import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
-public class RegistrationTests extends TestBase{
+public class RegistrationTests extends TestBase {
 
-  @BeforeMethod
+  //@BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
@@ -22,16 +20,16 @@ public class RegistrationTests extends TestBase{
   @Test
   public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String email = String.format("user%s@localhost", now);
-    String user = String.format ("user%s", now);
+    String user = String.format("user%s", now);
     String password = "password";
+    String email = String.format("user%s@localhost", now);
+    app.james().createUser(user, password);
     app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
-    app.newSession().login(user, password);
     assertTrue(app.newSession().login(user, password));
-
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
@@ -40,10 +38,8 @@ public class RegistrationTests extends TestBase{
     return regex.getText(mailMessage.text);
   }
 
-
-  @AfterMethod(alwaysRun = true)
+  //@AfterMethod(alwaysRun = true)
   public void stopMailServer() {
     app.mail().stop();
   }
-
 }
