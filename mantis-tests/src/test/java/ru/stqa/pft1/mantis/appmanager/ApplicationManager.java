91bd18335a7e4eaba3893e9826d4e68e28b7313f
YelinaY <eelina1205@gmail.com>
2019-03-30 23:56:12 +0200

    9.3. SOAP: взаимодействие с MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft1/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft1/mantis/appmanager/ApplicationManager.java
@@ -1,10 +1,9 @@
 package ru.stqa.pft1.mantis.appmanager;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
@@ -17,36 +16,47 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
   private final Properties properties;
   private WebDriver wd;
-  private DbHelper dbHelper;
-  private PasswordResetHelper resetHelper;
 
   private String browser;
   private RegistrationHelper registrationHelper;
-private FtpHelper ftp;
+  private FtpHelper ftp;
   private MailHelper mailHelper;
   private JamesHelper jamesHelper;
-
+  private PasswordResetHelper resetHelper;
+  private SessionHelper sessionHelper;
+  private DbHelper dbHelper;
+  private SoapHelper soapHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
     properties = new Properties();
-
-
   }
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     dbHelper = new DbHelper();
-
+  }
+  protected void click(By locator) {
+    wd.findElement(locator).click();
   }
   public void stop() {
     if (wd != null) {
       wd.quit();
     }
   }
-  public HttpSession newSession(){
+
+  public HttpSession newSession() {
     return new HttpSession(this);
+  }
+
+  public boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
   }
 
   public String getProperty(String key) {
@@ -63,40 +73,22 @@ private FtpHelper ftp;
   public FtpHelper ftp() {
     if (ftp == null) {
       ftp = new FtpHelper(this);
-    }return ftp;
-  }
-  public WebDriver getDriver() {
-    if (wd == null){
-
-      if (browser.equals(BrowserType.FIREFOX)) {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-      } else if (browser.equals(BrowserType.CHROME)) {
-        wd = new ChromeDriver();
-      } else if (browser.equals(BrowserType.IE)) {
-        wd = new InternetExplorerDriver();
-      }
-      wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-      wd.get(properties.getProperty("web.baseUrl"));
     }
-    return wd;
+    return ftp;
   }
-  public MailHelper mail(){
+
+  public MailHelper mail() {
     if (mailHelper == null) {
       mailHelper = new MailHelper(this);
     }
-      return mailHelper;
-    }
-public JamesHelper james (){
-    if (jamesHelper == null){
+    return mailHelper;
+  }
+  public JamesHelper james() {
+    if (jamesHelper == null) {
       jamesHelper = new JamesHelper(this);
     }
     return jamesHelper;
-}
-  public DbHelper db() {
-    return dbHelper;
   }
-
-
 
   public PasswordResetHelper resetHelper() {
     if (resetHelper == null) {
@@ -104,10 +96,39 @@ public JamesHelper james (){
     }
     return resetHelper;
   }
-  protected void click(By locator) {
-    wd.findElement(locator).click();
+
+  public DbHelper db() {
+    return dbHelper;
   }
 
+  public SessionHelper session() {
+    if(sessionHelper == null){
+      sessionHelper = new SessionHelper(this);
+    }
+    return sessionHelper;
+  }
+
+  public SoapHelper soap() {
+    if(soapHelper == null){
+      soapHelper = new SoapHelper(this);
+    }
+    return soapHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
+  }
   public void rootLogin() {
     wd.findElement(By.name("username")).sendKeys("administrator");
     wd.findElement(By.name("password")).sendKeys("root");
--- mantis-tests/src/test/java/ru/stqa/pft1/mantis/appmanager/HelperBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft1/mantis/appmanager/HelperBase.java
@@ -55,4 +55,5 @@ public class HelperBase {
       return false;
     }
   }
+
 }
